d702623e33642a300b8f7b467596e61c4dbe04bb
kanashiilove <nguyentranminhnguyen@gmail.com>
2025-11-04 14:32:41 -0800

    update

--- script/gemini-review.js
+++ script/gemini-review.js
@@ -43,7 +43,7 @@ const inlineFormat = `
 	[
 	{
 		"file": "filename.extension",
-		"line": <The position in the diff where you want to add a review comment. Note this value is not the same as the line number in the file. The position value equals the number of lines down from the first "@@" hunk header in the file you want to add a comment. The line just below the "@@" line is position 1, the next line is position 2, and so on. The position in the diff continues to increase through lines of whitespace and additional hunks until the beginning of a new file. Count from the top of the diff patch (first line = position 1)>,
+		"line": <The position in the diff where you want to add a review comment. Note this value is not the same as the line number in the file. The position value equals the number of lines down from the first "@@" hunk header in the file you want to add a comment. The line just below the "@@" line is position 1, the next line is position 2, and so on. The position in the diff continues to increase through lines of whitespace and additional hunks until the beginning of a new file.>,
 		"comment": "clear actionable feedback"
 	}
 	]
@@ -187,7 +187,7 @@ async function main() {
 					commit_id: commitSha,
 					comments: inlineComments.map((c) => ({
 						path: c.file,
-						position: c.line,
+						position: c.line + 1,
 						body: `💡 ${c.comment}`,
 					})),
 				});
